a49e451712c729c9a666e25443d0914e48a13918
Oshan Kottege <oshan.kottege@gmail.com>
2018-06-28 22:04:17 +1200

    Started implementing the service endpoints.

---
+++ notes-app-api/create.js
@@ -0,0 +1,33 @@
+import uuid from 'uuid';
+import AWS from 'aws-sdk';
+
+AWS.config.update({ region: 'us-west-1' });
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+    // Request body is passed in as JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: 'notes',
+        // 'Item' contains the attributes of the item to be created
+        // - 'userId': user identities are federated through the
+        //             Cognito Identity Pool, we will use the identity id
+        //             as the user id of the authenticated user
+        // - 'noteId': a unique uuid
+        // - 'content': parsed from request body
+        // - 'attachment': parsed from request body
+        // - 'createdAt': current Unix timestamp
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+        }
+    };
+
+    dynamodb.put(params, (error, data) => {
+
+    });
+}
