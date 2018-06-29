6394ccb3bd15bf3536ab74c3e1b7c6fab2782e5a
Oshan Kottege <oshan.kottege@myob.com>
2018-06-29 17:43:18 +1200

    Started testing the function locally.

--- notes-app-api/create.js
+++ notes-app-api/create.js
@@ -27,7 +27,42 @@ export function main(event, context, callback) {
         }
     };
 
-    dynamodb.put(params, (error, data) => {
+    const headers = {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true
+            }
 
-    });
+    dynamodb.put(params, (error, data) => {
+        // Set response headers to enable CORS
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        }
+
+        // Return status code 500 on error
+        if(error) {
+            const response = {
+                statusCode: 500,
+                headers: headers,
+                body: JSON.stringify({ status: false, error })
+            };
+            callback(data, response);
+            return;
+        }
+
+        // Return status code 200 and the newly created item
+        const response = {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify(params.Item)
+        };
+        callback(null, response);
+    });  
+
+    // const response = {
+    //         statusCode: 200,
+    //         headers,
+    //         body: JSON.stringify({})
+    //     };
+    //     callback(null, response);  
 }
