1d2869770afa36d572436f18db61bd5a8f5492a6
Oshan Kottege <oshan.kottege@gmail.com>
2018-06-29 21:20:32 +1200

    Added the create endpoint and got it working.

--- notes-app-api/create.js
+++ notes-app-api/create.js
@@ -1,23 +1,13 @@
 import uuid from 'uuid';
-import AWS from 'aws-sdk';
+import * as dynamoDbLib from './lib/dynamodb-lib';
+import { success, failure } from './lib/response-lib';
 
-AWS.config.update({ region: 'us-west-1' });
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context, callback) {
     // Request body is passed in as JSON encoded string in 'event.body'
     const data = JSON.parse(event.body);
 
     const params = {
         TableName: 'notes',
-        // 'Item' contains the attributes of the item to be created
-        // - 'userId': user identities are federated through the
-        //             Cognito Identity Pool, we will use the identity id
-        //             as the user id of the authenticated user
-        // - 'noteId': a unique uuid
-        // - 'content': parsed from request body
-        // - 'attachment': parsed from request body
-        // - 'createdAt': current Unix timestamp
         Item: {
             userId: event.requestContext.identity.cognitoIdentityId,
             noteId: uuid.v1(),
@@ -27,42 +17,11 @@ export function main(event, context, callback) {
         }
     };
 
-    const headers = {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": true
-            }
-
-    dynamodb.put(params, (error, data) => {
-        // Set response headers to enable CORS
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        }
-
-        // Return status code 500 on error
-        if(error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false, error })
-            };
-            callback(data, response);
-            return;
-        }
-
-        // Return status code 200 and the newly created item
-        const response = {
-            statusCode: 200,
-            headers,
-            body: JSON.stringify(params.Item)
-        };
-        callback(null, response);
-    });  
-
-    // const response = {
-    //         statusCode: 200,
-    //         headers,
-    //         body: JSON.stringify({})
-    //     };
-    //     callback(null, response);  
+    try {
+        await dynamoDbLib.call('put', params);
+        callback(null, success(params.Item));
+    } catch (e) {
+        console.log('ERROR: ', e);
+        callback(null, failure({ status: false }));
+    }
 }
