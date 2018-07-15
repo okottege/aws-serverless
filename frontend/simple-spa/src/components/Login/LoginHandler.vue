26be7f9c603ca40ed06c8b152d4c6935272846c7
Oshan Kottege <oshan.kottege@gmail.com>
2018-07-15 21:15:26 +1200

    Started working with the built-in login pages.

--- frontend/simple-spa/src/components/Login/LoginHandler.vue
+++ frontend/simple-spa/src/components/Login/LoginHandler.vue
@@ -7,12 +7,16 @@ export default {
   mounted() {
     console.log('checking tokens in the redirect url...');
 
-    const idToken = this.$route.query.id_token;
-    const accessToken = this.$route.query.access_token;
+    const accessToken = this.$route.hash.match(/(?:access_token)=([^&]+)/);
+    const idToken = this.$route.hash.match(/(?:id_token)=([^&]+)/);
+    const verification = this.$route.hash.match(/(?:state)=([^&]+)/);
+    const expiresIn = this.$route.hash.match(/(?:expires_in)=(\d+)/);
 
     if (idToken && accessToken) {
       console.log('id token: ', idToken);
       console.log('access token: ', accessToken);
+      console.log('state: ', verification);
+      console.log('expires in: ', expiresIn);
     } else {
       console.log('No tokens were found.');
     }
