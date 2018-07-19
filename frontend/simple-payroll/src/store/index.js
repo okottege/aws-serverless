46fe69d8dd21d96acfcf6c45ebd64a598acadea6
Oshan Kottege <oshan.kottege@gmail.com>
2018-07-20 06:03:04 +1200

    Started coding the store for login process.

--- frontend/simple-payroll/src/store/index.js
+++ frontend/simple-payroll/src/store/index.js
@@ -3,4 +3,38 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+/* eslint-disable no-param-reassign  */
+
+const LOGIN = 'LOGIN';
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+const LOGIN_FAILURE = 'LOGIN_FAILURE';
+const LOGOUT = 'LOGOUT';
+
+const state = {
+  isLoggedIn: false,
+};
+
+const mutations = {
+  [LOGIN](appState) {
+    appState.pending = true;
+  },
+
+  [LOGIN_SUCCESS](appState) {
+    appState.isLoggedIn = true;
+    appState.pending = false;
+  },
+
+  [LOGOUT](appState) {
+    appState.isLoggedIn = false;
+  },
+
+  [LOGIN_FAILURE](appState) {
+    appState.pending = false;
+  },
+};
+
+const actions = {
+
+};
+
 export default new Vuex.Store({});
