351d836cf2c9a8b25d8d63491cb5983b8ffe0058
Oshan Kottege <oshan.kottege@gmail.com>
2018-07-21 11:09:56 +1200

    Added logic to guard the views that user needs to be logged in.

--- frontend/simple-payroll/src/router/index.js
+++ frontend/simple-payroll/src/router/index.js
@@ -1,16 +1,42 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { Auth } from 'aws-amplify';
+
 import Home from '@/components/Home/Home';
 import SignIn from '@/components/Login/SignIn';
 import Dashboard from '@/components/Home/Dashboard';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     { path: '/', name: 'Home', component: Home },
     { path: '/login', name: 'Login', component: SignIn },
-    { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        authRequired: true,
+      },
+    },
   ],
 });
+
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.authRequired) {
+    const user = await Auth.currentUserInfo();
+    console.log('Logged in user is: ', JSON.stringify(user));
+
+    if (user) {
+      next();
+    } else {
+      next('/login');
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
